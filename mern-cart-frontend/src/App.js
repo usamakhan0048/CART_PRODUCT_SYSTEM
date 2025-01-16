f8a167299f4eebe36becec996c020a8ada08fce5
USAMA <uk515130@gmail.com>
2025-01-16 16:58:13 +0500

    first commit

--- mern-cart-frontend/src/App.js
+++ mern-cart-frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ProductList from './components/ProductList';
-import Cart from './components/Cart';
+import Cart from './components/Cart'; 
 
 function App() {
     const refreshCart = () => {
